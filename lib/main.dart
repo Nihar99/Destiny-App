8cddf004419851363e1206d231a6a19c59e0b224
Nihar99 <nihar.nyk1999@gmail.com>
2020-05-03 10:00:43 +0530

    Added story and story_brain classes

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:destinyapp/story_brain.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(Destiny());
@@ -5,7 +6,10 @@ void main() => runApp(Destiny());
 class Destiny extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      theme: ThemeData.dark(),
+      home: DestinyPage(),
+    );
   }
 }
 
@@ -15,8 +19,75 @@ class DestinyPage extends StatefulWidget {
 }
 
 class _DestinyPageState extends State<DestinyPage> {
+  StoryBrain storyBrain = new StoryBrain();
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('image/background.png'),
+          ),
+        ),
+        padding: EdgeInsets.symmetric(vertical: 50.0, horizontal: 15.0),
+        constraints: BoxConstraints.expand(),
+        child: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Expanded(
+                flex: 12,
+                child: Center(
+                  child: Text(
+                    storyBrain.getStory(),
+                    style: TextStyle(
+                      fontSize: 25.0,
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                flex: 2,
+                child: FlatButton(
+                  onPressed: () {
+                    //Choice 1 made by user.
+                    storyBrain.nextStory(1);
+                  },
+                  color: Colors.red,
+                  child: Text(
+                    storyBrain.getChoice1(),
+                    style: TextStyle(
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              Expanded(
+                flex: 2,
+                //TODO: Step 26 - Use a Flutter Visibility Widget to wrap this FlatButton.
+                //TODO: Step 28 - Set the "visible" property of the Visibility Widget to equal the output from the buttonShouldBeVisible() method in the storyBrain.
+                child: FlatButton(
+                  onPressed: () {
+                    //Choice 2 made by user.
+                    storyBrain.nextStory(2);
+                  },
+                  color: Colors.blue,
+                  child: Text(
+                    storyBrain.getChoice2(),
+                    style: TextStyle(
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
